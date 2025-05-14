653bacf8113a30ca4ff5159f6c1983c8e6c556ef
p-eight <pedrobonifa@gmail.com>
2025-05-14 19:59:02 -0300

    Modifies ILogger and implementation.

--- server/ConsoleLogger.hpp
+++ server/ConsoleLogger.hpp
@@ -4,15 +4,30 @@
 
 class ConsoleLogger : public ILogger {
 public:
-	void log(LogLevel level, const std::string& message) override {
+	
+	void log(LogLevel level, std::string_view message) override {
 		std::cout << "[" << level_to_string(level) << "] " << message << std::endl;
 	}
 
-	void debug(const std::string& message) override { log(LogLevel::Debug, message); }
-	void trace(const std::string& message) override { log(LogLevel::Trace, message); }
-	void info(const std::string& message)  override { log(LogLevel::Info, message); }
-	void error(const std::string& message) override { log(LogLevel::Error, message); }
-	void critical(const std::string& message) override { log(LogLevel::Critical, message); }
+	void trace(std::string_view message) override {
+		log(LogLevel::Trace, message);
+	}
+
+	void debug(std::string_view message) override {
+		log(LogLevel::Debug, message);
+	}
+
+	void info(std::string_view message) override {
+		log(LogLevel::Info, message);
+	}
+
+	void error(std::string_view message) override {
+		log(LogLevel::Error, message);
+	}
+
+	void critical(std::string_view message) override {
+		log(LogLevel::Critical, message);
+	}
 
 private:
 	std::string level_to_string(LogLevel level) {
--- server/Core/ILogger.hpp
+++ server/Core/ILogger.hpp
@@ -1,22 +1,53 @@
 #pragma once
 #include <string>
+#include <string_view>
+#include <fmt/core.h>
 
 class ILogger {
 public:
-
-	enum class LogLevel {
-		Trace,
-		Debug,
-		Info,
-		Error,
-		Critical
-	};
+    enum class LogLevel {
+        Trace,
+        Debug,
+        Info,
+        Error,
+        Critical
+    };
 
     virtual ~ILogger() = default;
-    virtual void log(LogLevel level, const std::string& message) = 0;
-	virtual void debug(const std::string& message) = 0;
-	virtual void trace(const std::string& message) = 0;
-	virtual void info(const std::string& message) = 0;
-	virtual void error(const std::string& message) = 0;
-	virtual void critical(const std::string& message) = 0;
+
+    // Base method
+    virtual void log(LogLevel level, std::string_view message) = 0;
+
+    // Overloads accepting dynamic strings
+    virtual void trace(std::string_view message) = 0;
+    virtual void debug(std::string_view message) = 0;
+    virtual void info(std::string_view message) = 0;
+    virtual void error(std::string_view message) = 0;
+    virtual void critical(std::string_view message) = 0;
+
+    // Overloads with fmt compile-time checked formatting
+    template<typename... Args>
+    void trace(fmt::format_string<Args...> fmt, Args&&... args) {
+        trace(fmt::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    void debug(fmt::format_string<Args...> fmt, Args&&... args) {
+        debug(fmt::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    void info(fmt::format_string<Args...> fmt, Args&&... args) {
+        info(fmt::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    void error(fmt::format_string<Args...> fmt, Args&&... args) {
+        error(fmt::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename... Args>
+    void critical(fmt::format_string<Args...> fmt, Args&&... args) {
+        critical(fmt::format(fmt, std::forward<Args>(args)...));
+    }
 };
